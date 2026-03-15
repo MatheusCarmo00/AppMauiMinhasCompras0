cbe1fca543b6c8039c46cb254ccc12a4538f7e51
Matheus do Carmo <m.mati.carmo@gmail.com>
2026-03-15 15:41:08 -0300

    utilizando SearchBar e ObservableCollection no desenvolvimento de aplicativos com .NET MAUI.

--- AppMauiMinhasCompras0/Views/ListaProduto.xaml.cs
+++ AppMauiMinhasCompras0/Views/ListaProduto.xaml.cs
@@ -1,21 +1,62 @@
+using AppMauiMinhasCompras0.Models;
+using System.Collections.ObjectModel;
+
 namespace AppMauiMinhasCompras0.Views;
+
 
 public partial class ListaProdutos : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
 	public ListaProdutos()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
+    }
+
+	protected async override void OnAppearing()
+	{
+		List<Produto> temp = await App.Db.GetAll();
+
+		temp.ForEach(i => lista.Add(i));
 	}
 
-	private void ToolbarItem_Clicked(object sender, EventArgs e)
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
 	{
-		try
-		{
-			Navigation.PushAsync(new Views.NovoProduto());
-		}
-		catch (Exception ex)
-		{
-			DisplayAlert("Ops", ex.Message, "OK");
-		}
-	}
+        try
+        {
+            Navigation.PushAsync(new Views.NovoProduto());
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> temp = await App.Db.Search(q);
+
+        temp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:C}";
+
+		DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
